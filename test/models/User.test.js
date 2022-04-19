39fd7ae8cf2b275aa7fed32fc4395a2d40af1ae6
Alexis Guzman <alexis12989x@gmail.com>
2022-04-19 16:07:35 -0500

    feat: add test for setters

--- test/models/User.test.js
+++ test/models/User.test.js
@@ -2,14 +2,7 @@ const User = require('./../../app/models/User');
 
 describe('Unit Tests for User class', () => {
   test('Create an User object', () => {
-    const user = new User(
-      1,
-      'carlogilmar',
-      'Carlo',
-      'Bio',
-      'dateCreated',
-      'lastUpdated'
-    );
+    const user = new User(1, 'carlogilmar', 'Carlo', 'Bio');
 
     expect(user.id).toBe(1);
     expect(user.username).toBe('carlogilmar');
@@ -17,5 +10,23 @@ describe('Unit Tests for User class', () => {
     expect(user.bio).toBe('Bio');
     expect(user.dateCreated).not.toBeUndefined();
     expect(user.lastUpdated).not.toBeUndefined();
+  });
+  test('Add getters', () => {
+    const user = new User(1, 'carlogilmar', 'Carlo', 'Bio');
+
+    expect(user.id).toBe(1);
+    expect(user.getUsername).toBe('carlogilmar');
+    expect(user.getBio).toBe('Bio');
+    expect(user.getDateCreated).not.toBeUndefined();
+    expect(user.getLastUpdated).not.toBeUndefined();
+  });
+  test('Add setters', () => {
+    const user = new User(1, 'carlogilmar', 'Carlo', 'Bio');
+
+    user.setUsername = 'Gilmar';
+    expect(user.username).toBe('Gilmar');
+
+    user.setBio = 'New bio';
+    expect(user.bio).toBe('New bio');
   });
 });
