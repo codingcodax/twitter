4b301b447d5085c15af6381a2998886b8b94a49b
Alexis Guzman <alexis12989x@gmail.com>
2022-04-20 12:06:38 -0500

    fix: use create funtion instead of new keyword

--- test/services/UserService.test.js
+++ test/services/UserService.test.js
@@ -19,19 +19,19 @@ describe('Tests for UserService', () => {
     expect(userInfoList[3]).toBe('Sin bio');
   });
   test('3. Update username', () => {
-    const user = new UserService(1, 'carlogilmar', 'Carlo');
+    const user = UserService.create(1, 'carlogilmar', 'Carlo');
     UserService.updateUserUsername(user, 'carlog');
 
     expect(user.username).toBe('carlog');
   });
-  test('3. Given a list of users give me the list of usernames', () => {
-    const user1 = new UserService(1, 'carlogilmar', 'Carlo');
-    const user2 = new UserService(2, 'codingodax', 'Alexis');
-    const user3 = new UserService(3, 'codaxx', 'Alexis');
+  test('4. Given a list of users give me the list of usernames', () => {
+    const user1 = UserService.create(1, 'carlogilmar', 'Carlo');
+    const user2 = UserService.create(2, 'codingcodax', 'Alexis');
+    const user3 = UserService.create(3, 'codaxx', 'Alexis');
     const usernames = UserService.getAllUsernames([user1, user2, user3]);
 
     expect(usernames).toContain('carlogilmar');
-    expect(usernames).toContain('codingodax');
+    expect(usernames).toContain('codingcodax');
     expect(usernames).toContain('codaxx');
   });
 });
